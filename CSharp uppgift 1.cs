ef442cf681e3b722bd86fd247cff9eadba1e3610
NoelBodestig <91761994+NoelBodestig@users.noreply.github.com>
2022-08-26 14:27:11 +0200

    Add files via upload

---
+++ CSharp uppgift 1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CSharp_2
+{
+    using System;
+
+    namespace exercise_04
+    {
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Hello world");
+
+                Console.WriteLine("Once upon a time");
+                Console.WriteLine("I was falling in love");
+                Console.WriteLine("Now I’m only falling apart");
+
+                string text = "Live and sleep";
+                Console.WriteLine(text);
+                string name = "Ada Lovelace";
+                Console.WriteLine(name);
+
+                string svar = Console.ReadLine();
+                Console.WriteLine(svar);
+
+                string tre = Console.ReadLine();
+                Console.WriteLine(tre);
+                Console.WriteLine(tre);
+                Console.WriteLine(tre);
+
+                Console.WriteLine("Vad heter du?");
+                string fråga = Console.ReadLine();
+                Console.WriteLine(fråga);
+                Console.WriteLine("Välkommen "+ fråga);
+
+                Console.WriteLine("Hej hur är det? ");
+                string fråga1 = Console.ReadLine();
+                Console.WriteLine(fråga1);
+                Console.WriteLine("Bra, berätta mer " + fråga);
+                string fråga2 = Console.ReadLine();
+                Console.WriteLine(fråga2);
+                Console.WriteLine("Intressant " + fråga);
+
+
+
+            }
+
+
+
+        }
+    }
+}
